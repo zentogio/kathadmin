5e20c59a42db9fa5aaa72b1b70c2382255069d4b
zentogio <zentogioeiei@gmail.com>
2026-08-10 22:50:25 +0700

    k

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
-	adapter: node({ mode: 'standalone' }),
+	adapter: vercel(),
 	server: { port: 4322 },
 });
